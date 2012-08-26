05586a8f07533b99b8f4d14162d8030ed13246ac
Jake Harding <jacob.s.harding@gmail.com>
2012-08-26 01:38:06 -0700

    Initial commit.

---
+++ server.js
@@ -0,0 +1,53 @@
+// ilk: message boards
+// -------------------
+// * Copyright (c) 2012 Jake Harding
+
+// module dependencies
+// ===================
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// configuration and middleware
+// ============================
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hjs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'assets')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+  app.use(require('less-middleware')({
+    force: true,
+    debug: true,
+    compress: false,
+    src: __dirname + '/src/less',
+    dest: __dirname + '/assets/css'
+  }));
+});
+
+// routes
+// ======
+
+app.get('/', routes.index);
+
+// start your engines
+// ==================
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
