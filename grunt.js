7e238f28a270fa9426fd97271d3e9b804f479d5c
Jake Harding <jacob.s.harding@gmail.com>
2012-11-03 23:25:09 -0700

    Add tasks to create/drop db.

--- grunt.js
+++ grunt.js
@@ -1,42 +1,60 @@
+var path = require('path');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     lint: {
-      client: ['src/js/**/*.js'],
-      server: ['./*.js', './routes/**/*.js', './models/**/*.js']
-    },
-    less: {
+      client: ['src/js/**/*.js']
+    , server: ['./*.js', './routes/**/*.js', './models/**/*.js']
+    }
+  , less: {
       development: {
         files: {
           'assets/css/style.css': 'src/less/style.less'
         }
       }
-    },
-    watch: {
-      files: 'src/less/**/*.less',
-      tasks: 'less:style'
-    },
-    jshint: {
+    }
+  , watch: {
+      files: 'src/less/**/*.less'
+    , tasks: 'less:style'
+    }
+  , jshint: {
       options: {
         // enforcing options
-        bitwise: true,
-        curly: true,
-        newcap: true,
-        noarg: true,
-        noempty: true,
-        nonew: true,
-        trailing: true,
+        bitwise: true
+      , curly: true
+      , newcap: true
+      , noarg: true
+      , noempty: true
+      , nonew: true
+      , trailing: true
         // relaxing options
-        boss: true,
-        es5: true,
-        evil: true,
-        expr: true,
+      , boss: true
+      , es5: true
+      , evil: true
+      , expr: true
         // environments
-        node: true
+      , node: true
+      }
+    }
+  , exec: {
+      dbcreate: {
+        command: 'psql -h localhost -d ilk -f ' +
+          path.join(__dirname, '/db/create_tables.sql')
+      , stdout: true
+      }
+    , dbdrop: {
+        command: 'psql -h localhost -d ilk -f ' +
+          path.join(__dirname, '/db/drop_tables.sql')
+      , stdout: true
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('dbcreate', 'exec:dbcreate');
+  grunt.registerTask('dbdrop', 'exec:dbdrop');
 
   grunt.registerTask('default', 'lint');
 };
