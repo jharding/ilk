5b18fa69176a039e5473b0dcfd2a620ba1dd1082
Jake Harding <jacob.s.harding@gmail.com>
2012-11-04 16:00:10 -0800

    Add simple login.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 // dependencies
 // ------------
 
-var app = require('../server')
-  , mysql = app.get('mysql')
+var _ = require('underscore')
+  , app = require('../server')
+  , db = app.get('mysql')
+  , mysql = require('mysql')
   , bcrypt = require('bcrypt')
   , fabio = require('fabio');
 
@@ -10,9 +12,12 @@ var app = require('../server')
 // ----------------
 
 var User
-  , rounds = 10;
+  , bcryptRounds = 10;
 
 User = module.exports = fabio.define({
+  // schema
+  // ------
+
   schema: {
     email: {
       validators: fabio.validators.isFuzzyEmail
@@ -23,36 +28,83 @@ User = module.exports = fabio.define({
     }
   }
 
-, statics: {}
+  // statics
+  // -------
+
+, statics: {
+    findOne: function(attrs, cb) {
+      var query = composeSelect('users', attrs)
+        , user;
+
+      db.query(query, function(err, results) {
+        if (err) { return cb(err); }
+
+        cb(null,  results[0] ? new User(results[0], { raw: true }) : null);
+      });
+    }
+  }
+
+  // methods
+  // -------
 
 , methods: {
     create: function(attrs, cb) {
       var query = 'INSERT INTO users SET ?';
 
-      mysql.query(query, attrs, function(err, results) {
-        if (err) { cb(err); return; }
+      db.query(query, attrs, function(err, results) {
+        if (err) { return cb(err); }
 
+        // TODO: pass something
         cb(null);
       });
-
-      mysql.end();
     }
 
   , update: function(attrs, cb) {
-      console.log('update');
-      cb(null, attrs);
+      // TODO
     }
 
-  , delete: function(attrs, cb) {}
-}
+  , delete: function(attrs, cb) {
+      // TODO
+    }
+
+  , verifyPassword: function(val, cb) {
+      bcrypt.compare(val, this.getAttrs().password, function(err, same) {
+        if (err) { return cb(err); }
+
+        cb(null, same);
+      });
+    }
+  }
 });
 
 // helper functions
 // ----------------
 
+function composeSelect(tables, conditions) {
+  var statement
+    , keys;
+
+  tables = _.isArray(tables) ? tables.join(', ') : tables;
+  statement = 'SELECT * FROM ' + tables;
+
+  keys = Object.keys(conditions || {});
+
+  if (keys.length > 0) {
+    statement += ' WHERE ';
+    keys.forEach(function(key) {
+      statement += key + '=' + mysql.escape(conditions[key]) + ' AND ';
+    });
+
+    // strip trailing AND
+    statement = statement.replace(/ AND $/, ' ');
+  }
+
+  return statement;
+}
+
 function hash(val, cb) {
-  bcrypt.hash(val, rounds, function(err, hashedVal) {
-    if (err) { cb(err); return; }
+  bcrypt.hash(val, bcryptRounds, function(err, hashedVal) {
+    if (err) { return cb(err); }
 
     cb(null, hashedVal);
   });
--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,41 @@
 // dependencies
 // ------------
 
-var User = require('../models/user.js');
+var User = require('../models/user');
 
 // pages
 // -----
 
 module.exports.pages = {
   signup: function(req, res, next) { res.render('user/signup'); }
+, login: function(req, res, next) { res.render('user/login'); }
 };
 
 module.exports.actions = {
-  register: function(req, res, next) {
+  signup: function(req, res, next) {
     new User({
       email: req.body.email
     , password: req.body.password
     })
     .save()
-    .val(function(attrs) {
+    .value(function(attrs) {
       res.redirect('/');
     })
     .error(function(err) { next(err); });
+  }
+
+, login: function(req, res, next) {
+    var conditions = { email: req.body.email };
+
+    User.findOne(conditions, function(err, user) {
+      if (err) { return next(err); }
+
+      user
+      .verifyPassword(req.body.password)
+      .value(function(verified) {
+        res.redirect('/');
+      })
+      .error(function(err) { next(err); });
+    });
   }
 };
